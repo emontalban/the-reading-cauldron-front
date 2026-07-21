9799bfed404c3b02b383279b0bd6441385b44ae9
emontalban <mentulbenodarna@gmail.com>
2026-07-21 20:48:22 +0200

    añadido la pagina de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router"
 import LoginPage from "./pages/LoginPage"
-
+import RegisterPage from "./pages/RegisterPage"
 function App(){
     return(
         
@@ -9,10 +9,12 @@ function App(){
             <nav className="navbar">
                 <Link to= "/">The Reading Cauldron</Link>
                 <Link to= "/login">Login</Link>
+                <Link to="/register">Registro</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<h1>Bienvenido a The Reading Cauldrom</h1>}></Route>
                 <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage />} />
             </Routes>
             </div>  
         </BrowserRouter>
